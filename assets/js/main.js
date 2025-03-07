d9792eec9517d5f45f73ce51d64b6fb274ff272d
Roberto Loria <Loria.roberto97@gmail.com>
2025-03-07 16:43:25 +0100

    Edit code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 // L'utente clicca su un bottone 
-    //Craere un bottone su html 
-    // selezioniamo il  bottone 
-    // Al clic attraverso addventlistner il bottone deve
-    // Generare una grglia 
+//Craere un bottone su html 
+// selezioniamo il  bottone 
+// Al clic attraverso addventlistner il bottone deve
+// Generare una grglia 
 
 
 
@@ -20,35 +20,30 @@ const gridElement = document.querySelector(`.grid`);
 // Una variabile con i numeri delle celle 
 let cellNumbers = 100
 
+// Creo un Array vuota dove inserire al suo internno dei numeri casuali
+const bombs = []
+
 
 // Al clic attraverso addventlistner il bottone deve generare un griglia 
 // Ogni cella ha un numero progressivo, da 1 a 100.
-    // Generiamo dei numeri da 1 a 100 
+// Generiamo dei numeri da 1 a 100 
 
-buttonElement.addEventListener(`click`,function () {
-    // console.log(`Ho cliccato sul pulsante`);
+buttonElement.addEventListener('click', function () {
+    // Puliamo la griglia prima di rigenerarla
+    gridElement.innerHTML = '';
 
+    // Generiamo la griglia
     for (let i = 1; i <= cellNumbers; i++) {
-        // console.log(cellNumbers);
-        // creiamo il nostro markup attraverso una costante  da inserire nella dom 
-        let cellElement = `<div class="cell">${i}</div>`
-        // Utilizziamo insertAd
-        gridElement.insertAdjacentHTML('beforeend', cellElement); 
-    }    
-    // Quando l'utente cliccherà sulla cella essa si deve colorare di blu
-       // selezionare la singola cella
-       // vedere se la cessa selezionata corrisponda 
-       const singleCell = document.querySelector(`.cell`);
-       // console.log(singleCell);
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.textContent = i;
 
-       // utliazziamo un eventListner 
-         // aggiungo un event listener alla cella selezionata
-        singleCell.addEventListener(`click`,function () {
-        console.log(`ho cliccato`);
-         // al click la cella si colorerà di blu
-        singleCell.classList.toggle(`lightblue`)
-        })  
+        // Al click, cambia colore e stampa il numero
+        cell.addEventListener('click', function () {
+            cell.classList.toggle('lightblue');
+            console.log(`Hai cliccato sulla cella numero ${i}`);
+        });
 
-})    
-
-    
+        gridElement.appendChild(cell);
+    }
+});
